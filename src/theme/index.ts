dd33a8f0a4ba3325d89643e4c5eb494481919b0a
Paardhu22 <paardhivreddy22@gmail.com>
2026-07-14 23:03:02 +0530

    feat: add theme barrel export
    Re-exports all design tokens from @/theme.

---
+++ src/theme/index.ts
@@ -0,0 +1,7 @@
+export { colors } from './colors';
+export type { ColorToken } from './colors';
+
+export { typography } from './typography';
+export type { TypographyVariant } from './typography';
+
+export { spacing, borderRadius } from './spacing';
