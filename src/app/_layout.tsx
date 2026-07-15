a0c1c6fa1dd5737852c2ee438e67515a8c58b9b4
Paardhu22 <paardhivreddy22@gmail.com>
2026-07-15 14:54:36 +0530

    Update _layout.tsx

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,18 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 
+import { AuthProvider, PropertyProvider } from '@/store';
+import { ThemeProvider } from '@/theme';
+
 export default function RootLayout() {
   return (
-    <>
-      <StatusBar style="dark" />
-      <Stack screenOptions={{ headerShown: false }} />
-    </>
+    <ThemeProvider>
+      <AuthProvider>
+        <PropertyProvider>
+          <StatusBar style="dark" />
+          <Stack screenOptions={{ headerShown: false }} />
+        </PropertyProvider>
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
